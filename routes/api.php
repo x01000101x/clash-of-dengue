8056b49e9d1ee1982144babfc10b041b3a2fa11c
Leo <leonarddamanik7@gmail.com>
2024-11-01 00:45:56 +0700

    init login, register, logout, and tokenization

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/auth/test', [UserController::class, 'test']);
+    Route::post('/auth/logout', [UserController::class, 'logoutUser']);
+
 });
+
+
+Route::post('/auth/register', [UserController::class, 'createUser']);
+Route::post('/auth/login', [UserController::class, 'loginUser']);
