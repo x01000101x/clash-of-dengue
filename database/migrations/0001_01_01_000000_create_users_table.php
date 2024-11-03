a7f51cb5f71c135abd19825e52569d9286ed49f5
Leo <leonarddamanik7@gmail.com>
2024-11-03 14:16:08 +0700

    create session

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('session_id');
             $table->string('name');
             $table->string('username')->unique();
             $table->string('school_name')->nullable();
